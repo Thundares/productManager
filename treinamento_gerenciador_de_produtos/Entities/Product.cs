c3a23bea32a94288e644330b8bc46672d4e89308
Rafael Mippo <rkmippo@gmail.com>
2019-08-13 17:21:55 -0300

    Price was made a Private float with get and set implemented // A Test() method was made and all results were successful

--- treinamento_gerenciador_de_produtos/Entities/Product.cs
+++ treinamento_gerenciador_de_produtos/Entities/Product.cs
@@ -5,28 +5,45 @@ namespace treinamento_gerenciador_de_produtos.Entities
 {
     class Product
     {
+        //privates
+        private float _price;
+        //end of private
+
         //properties
         public ushort Id { get; private set; }
         public string Name { get; private set; }
-        public ushort Quantity { get; set; }
-        public float Price { get; set; }
+        public ushort Quantity { get; private set; }
         //end of properties
 
         //contructor
-        Product(ushort id, string name)
+        public Product(ushort id, string name)
         {
             Id = id;
             Name = name;
         }
-        Product(ushort id, string name, ushort quantity) : this( id, name)
+        public Product(ushort id, string name, ushort quantity) : this(id, name)
         {
             Quantity = quantity;
         }
-        Product(ushort id, string name, ushort quantity, float price) : this(id, name, quantity)
+        public Product(ushort id, string name, ushort quantity, float price) : this(id, name, quantity)
         {
-            Price = price;
+            _price = price;
         }
         //end of constructor
+
+        //Custom prop
+        public float Price
+        {
+            get { return _price; }
+            set {
+                if (_price + value < 0.00f)
+                    Console.WriteLine("Erro 002: Product cannot have negative Price");
+                else
+                    _price += value;
+                }
+
+        }
+        //end of custom
 
         //methods
         public void Add(ushort i)
@@ -40,6 +57,7 @@ namespace treinamento_gerenciador_de_produtos.Entities
             else
                 Quantity -= i;
         }
+
         //end of methods
 
         //overrides
--- treinamento_gerenciador_de_produtos/Program.cs
+++ treinamento_gerenciador_de_produtos/Program.cs
@@ -22,7 +22,8 @@ namespace treinamento_gerenciador_de_produtos
             while (working)
             {
                 Menu(); //Whats is going to be shown
-                working = Control(); //input from the user
+                //working = Control(); //input from the user
+                Test();
                 Console.Clear();
             }
             //end of program loop
@@ -66,6 +67,29 @@ namespace treinamento_gerenciador_de_produtos
                 Console.WriteLine("To leave this programm type <quit>");
             }
             return true;
+        }
+
+        static void Test()
+        {
+            Console.WriteLine("Class Product");
+            Product Test;
+            Console.WriteLine("Test new Product");
+            Test = new Product((ushort)0001, "Teste" );
+            Console.WriteLine(Test);
+            Console.WriteLine("Test 2");
+            Product Test2 = new Product(0002, "Teste2", 1);
+            Console.WriteLine(Test2);
+            Console.WriteLine("Test 3");
+            Product Test3 = new Product(0003, "Teste3", 2, 51.30f);
+            Console.WriteLine(Test3);
+            Console.WriteLine();
+            Console.WriteLine("Add Test");
+            Test.Add(1);
+            Console.WriteLine(Test);
+            Console.WriteLine("Remove Test2");
+            Test2.Remove(1);
+            Console.WriteLine(Test2);
+            Console.ReadLine();
         }
     }
 }
