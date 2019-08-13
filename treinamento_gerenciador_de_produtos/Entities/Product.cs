c1f6acb5e56ea6a176166514a717e1bd89f0ce3f
Rafael Mippo <rkmippo@gmail.com>
2019-08-13 16:43:09 -0300

    Added Tostring() override // Add and Remove Methods

--- treinamento_gerenciador_de_produtos/Entities/Product.cs
+++ treinamento_gerenciador_de_produtos/Entities/Product.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace treinamento_gerenciador_de_produtos.Entities
 {
     class Product
@@ -16,18 +18,44 @@ namespace treinamento_gerenciador_de_produtos.Entities
             Id = id;
             Name = name;
         }
-
-        Product(ushort id, string name, ushort quantity)
+        Product(ushort id, string name, ushort quantity) : this( id, name)
         {
             Quantity = quantity;
+        }
+        Product(ushort id, string name, ushort quantity, float price) : this(id, name, quantity)
+        {
+            Price = price;
         }
         //end of constructor
 
         //methods
-
+        public void Add(ushort i)
+        {
+            Quantity += i;
+        }
+        public void Remove(ushort i)
+        {
+            if (i > Quantity)
+                Console.WriteLine("Erro 001: Quantity cannot be negative");
+            else
+                Quantity -= i;
+        }
         //end of methods
 
         //overrides
+        public override string ToString()
+        {
+            return "Id: " +
+                Id.ToString() +
+                Environment.NewLine +
+                "Name: " +
+                Name +
+                ". Quantity: " +
+                Quantity.ToString() +
+                Environment.NewLine +
+                "Price: " +
+                Price.ToString("F2");
+        }
         //end of overrides
     }
 }
